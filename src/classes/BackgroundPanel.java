d09121e9c24fb5918bfc444bf86c70db9662ec49
CIANDT\gustavofb <gustavofb@ciandt.com>
2023-05-26 22:08:15 -0300

    Subindo projeto

---
+++ src/classes/BackgroundPanel.java
@@ -0,0 +1,17 @@
+    package classes;
+import javax.swing.*;
+import java.awt.*;
+
+public class BackgroundPanel extends JPanel {
+    private Image backgroundImage;
+
+    public BackgroundPanel(Image backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+}
